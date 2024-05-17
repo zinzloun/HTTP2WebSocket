410d5317d6c101cb96c6e38fba99ed6ad34cfc54
Filippo *ZinzlouN* Bersani <filobers@protonmail.com>
2024-05-17 16:45:03 +0200

    Add support OPTIONS

--- HTTP2WebSocket.py
+++ HTTP2WebSocket.py
@@ -14,28 +14,45 @@ from websocket import create_connection, _exceptions
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import unquote
 
+
 class proxyServer(BaseHTTPRequestHandler):
     post_body = ''
 
     def do_POST(self):
-        self.path = '/'+self.path if self.path[0] != '/' else self.path # Fix broken paths
-        length = int(self.headers.get('Content-Length','0'))
+       
+        # Fix broken paths
+        self.path = '/'+self.path if self.path[0] != '/' else self.path
+        length = int(self.headers.get('Content-Length', '0'))
         self.post_body = self.rfile.read(length)
-        self.post_body = str(unquote(self.post_body.decode('utf-8'))) # URL decode
-        self.post_body = self.post_body[len(PARAMETER):] # Strip of the artificial parameter
+        self.post_body = str(
+            unquote(self.post_body.decode('utf-8')))  # URL decode
+        # Strip of the artificial parameter
+        self.post_body = self.post_body[len(PARAMETER):]
         if PARAMETER != '' and self.post_body[:len(PARAMETER)] == PARAMETER:
-            self.post_body = self.post_body[:-1] if '&' == self.post_body[-1] else self.post_body  # Strip off last & if parameter provided
+            # Strip off last & if parameter provided
+            self.post_body = self.post_body[:-
+                                            1] if '&' == self.post_body[-1] else self.post_body
         response = self.ws_request(self.path, self.post_body)
         self.http_response(response)
 
     def do_GET(self):
-        self.path = '/'+self.path if self.path[0] != '/' else self.path # Fix broken paths
+        # Fix broken paths
+        self.path = '/'+self.path if self.path[0] != '/' else self.path
         response = self.ws_request(self.path, '')
         self.http_response(response)
 
+    def do_OPTIONS(self):
+        
+        self.send_response(200, "ok")
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+        self.send_header("Access-Control-Allow-Headers", "X-Requested-With")
+        self.send_header("Access-Control-Allow-Headers", "Content-Type")
+        self.end_headers()
+
     def http_response(self, response):
         """Analyze websocket response and send http response to a client"""
-
+        
         if isinstance(response, (int, dict)):
             if response == 404:
                 self.send_response(404)
@@ -53,6 +70,8 @@ class proxyServer(BaseHTTPRequestHandler):
         self.send_header('Content-Type', 'text/html')
         self.end_headers()
         try:
+            # Print response in the console too
+            print(" |- RESPONSE --------> " + str(response))
             self.wfile.write(bytearray(response, 'utf-8'))
         except BrokenPipeError as error:
             print('[-] Client disconnected. '+str(error))
@@ -82,13 +101,14 @@ class proxyServer(BaseHTTPRequestHandler):
 
         # Check if correct prefix is provided
         if 'ws://' != target[:5] and 'wss://' != target[:6]:
-            error = 'Protocol not supported. Target {} has to have ws:// or wss:// protocol specified'.format(target)
+            error = 'Protocol not supported. Target {} has to have ws:// or wss:// protocol specified'.format(
+                target)
             print('[-] {}'.format(error))
             return {'error': error}
 
         if 'wss://' == target[:6]:
             if SECURE:
-                context={}
+                context = {}
             else:
                 context = {'cert_reqs': ssl.CERT_NONE, 'check_hostname': False}
         else:
@@ -100,11 +120,13 @@ class proxyServer(BaseHTTPRequestHandler):
         if CONNECTION is None or not CONNECTION.connected:
             try:
                 if proxy:
-                    CONNECTION = create_connection('{}{}'.format(target, url), http_proxy_host=proxy[0], http_proxy_port=proxy[1], sslopt=context, header=headers, timeout=WAIT)
+                    CONNECTION = create_connection('{}{}'.format(
+                        target, url), http_proxy_host=proxy[0], http_proxy_port=proxy[1], sslopt=context, header=headers, timeout=WAIT)
                     CONN_START = datetime.now()
                     sleep(DELAY)
                 else:
-                    CONNECTION = create_connection('{}{}'.format(target, url), sslopt=context, header=headers, timeout=WAIT)
+                    CONNECTION = create_connection('{}{}'.format(
+                        target, url), sslopt=context, header=headers, timeout=WAIT)
                     CONN_START = datetime.now()
                     sleep(DELAY)
             except _exceptions.WebSocketBadStatusException as error:
@@ -161,23 +183,34 @@ class proxyServer(BaseHTTPRequestHandler):
             prepared.append('{}: {}'.format(header, headers[header]))
 
         return prepared
-    
+
     def log_message(self, log_format, *args):
-        print('{} - - [{}] {}'.format(self.address_string(), self.log_date_time_string(), log_format%args))
+        print('{} - - [{}] {}'.format(self.address_string(),
+              self.log_date_time_string(), log_format % args))
         if self.post_body != '' and VERBOSE:
-            print('================== WS BODY START ==================\n{}\n==================  WS BODY END  =================='.format(self.post_body))
+            print('================== WS BODY START ==================\n{}\n==================  WS BODY END  =================='.format(
+                self.post_body))
+
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='HTTP2WebSocket is Python 3 Web Socket Proxy tool to translate HTTP traffic to WebSocket application. It allows to test WebSocket app with tools like sqlmap, dirb, commix. Headers sent by you will be passed to Web Socket application.')
-    parser.add_argument('-l','--listen', help='Local port to listen to.', type=int, required=True)
-    parser.add_argument('-t','--target', help='Your target WebSocket application (E.g.: ws://localhost:1234)')
-    parser.add_argument('-w', '--wait', help='Wait specific amount of seconds for WS responses', type=int, default=1)
-    parser.add_argument('-K', '--keep', help='Keep the connection open for provided amount of seconds', type=int, default=3)
-    parser.add_argument('-d', '--delay', help='The delay between opening a WS connection and sending a request', type=int, default=0)
-    parser.add_argument('-P','--parameter', help='Artificial parameter for POST body. Actual content sent will be the value of this parameter. Some tools (ex: fimap) don\'t work with plain body without any parameters.')
-    parser.add_argument('-v','--verbose', default=False, action='store_true', help='Shows HTTP POST body message.')
-    parser.add_argument('-k','--insecure', default=False, action='store_true', help='Don\'t verify SSL certificate.')
-    parser.add_argument('-p','--proxy', default=False, help='HTTP proxy URL')
+    parser = argparse.ArgumentParser(
+        description='HTTP2WebSocket is Python 3 Web Socket Proxy tool to translate HTTP traffic to WebSocket application. It allows to test WebSocket app with tools like sqlmap, dirb, commix. Headers sent by you will be passed to Web Socket application.')
+    parser.add_argument(
+        '-l', '--listen', help='Local port to listen to.', type=int, required=True)
+    parser.add_argument(
+        '-t', '--target', help='Your target WebSocket application (E.g.: ws://localhost:1234)')
+    parser.add_argument(
+        '-w', '--wait', help='Wait specific amount of seconds for WS responses', type=int, default=1)
+    parser.add_argument(
+        '-K', '--keep', help='Keep the connection open for provided amount of seconds', type=int, default=3)
+    parser.add_argument(
+        '-d', '--delay', help='The delay between opening a WS connection and sending a request', type=int, default=0)
+    parser.add_argument('-P', '--parameter', help='Artificial parameter for POST body. Actual content sent will be the value of this parameter. Some tools (ex: fimap) don\'t work with plain body without any parameters.')
+    parser.add_argument('-v', '--verbose', default=False,
+                        action='store_true', help='Shows HTTP POST body message.')
+    parser.add_argument('-k', '--insecure', default=False,
+                        action='store_true', help='Don\'t verify SSL certificate.')
+    parser.add_argument('-p', '--proxy', default=False, help='HTTP proxy URL')
     args = parser.parse_args()
 
     CONNECTION = None
